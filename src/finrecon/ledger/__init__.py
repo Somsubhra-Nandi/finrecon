fe0f9d796487c0ada9bbb9d0406584b000c0618b
Somsubhra Nandi <nruma325@gmail.com>
2026-08-28 10:59:02 +0530

    chore(ledger): export human resolution primitives
    Signed-off-by: Somsubhra Nandi <nruma325@gmail.com>

--- src/finrecon/ledger/__init__.py
+++ src/finrecon/ledger/__init__.py
@@ -3,6 +3,7 @@
 from .audit import audit_id_for, audit_payload, audit_row, canonical_json, is_resolution
 from .schema import SCHEMA_STATEMENTS, SCHEMA_VERSION
 from .store import BatchIdentityError, LedgerStore, open_ledger
+from .human import HumanResolution, HumanResolutionError
 
 __all__ = [
     "audit_id_for",
@@ -15,4 +16,6 @@ __all__ = [
     "BatchIdentityError",
     "LedgerStore",
     "open_ledger",
+    "HumanResolution",
+    "HumanResolutionError",
 ]
